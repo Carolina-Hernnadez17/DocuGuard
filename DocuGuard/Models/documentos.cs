d16c8d0b779e0cfb98ce3096a52f72d47b8cc20c
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-04-25 19:34:42 -0600

    subida de modelo de documentos

--- DocuGuard/Models/documentos.cs
+++ DocuGuard/Models/documentos.cs
@@ -1,6 +1,18 @@
-﻿namespace DocuGuard.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocuGuard.Models
 {
     public class documentos
     {
+        [Key]
+        public int id_documento { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string ruta_archivo { get; set; }
+        public DateTime fecha_subida {  get; set; }
+        public int version { get; set; }
+        public string estado { get; set; }
+       
     }
 }
